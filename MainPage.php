3c69f5f09a16837170c44d8bd1dd14e258ee878f
Denis1098 <Denis.r001@mail.ru>
2021-12-06 16:08:59 +0300

    Initial commit

---
+++ MainPage.php
@@ -0,0 +1,19 @@
+<?php
+ include "Templates/Header.php";
+?>
+<div class="container">
+    <p></p>
+</div>
+<div class="container">
+    <div class="card" style="width: 18rem;">
+        <!--img src="..." class="card-img-top" alt="..."-->
+        <div class="card-body">
+            <h5 class="card-title">Помещение</h5>
+            <p class="card-text">Супер помещение в центре Москвы. Замечательные тарифы!</p>
+            <a href="#" class="btn btn-primary">Подробнее</a>
+        </div>
+    </div>
+</div>
+<?php
+include "Templates/Footer.php";
+?>
